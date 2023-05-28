f6be95097a78513bf319109c3ac150a05b5f0596
canozben <canozben@windowslive.com>
2023-05-28 22:54:37 +0300

    Auth Setup Done Next: Auth UI

---
+++ app/(site)/page.tsx
@@ -0,0 +1,24 @@
+import { DiCssdeck } from 'react-icons/di';
+
+export default function Home() {
+  return (
+    <div
+      className="
+      flex flex-col min-h-full
+      bg-gray-100 justify-center py-12 sm:px-6 lg:px-8
+      ">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <DiCssdeck size="4rem"
+          height="48"
+          width="48"
+          className="mx-auto w-auto"
+        />
+        <h2 className=" mt-6 text-center text-3xl font-bold
+          tracking-tight text-gray-900">
+            Sign In
+        </h2>
+      </div>
+      {/* AuthForm */}
+    </div>
+  )
+}
