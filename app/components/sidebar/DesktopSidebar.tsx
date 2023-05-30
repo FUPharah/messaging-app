4a92bf2bf6e966839b73561371e4f93f7d3b6efe
canozben <canozben@windowslive.com>
2023-05-30 21:27:39 +0300

    layout, sidebar and navigation done online green light hardcoded gonna change later

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -2,11 +2,20 @@
 import { useState } from "react";
 import useRoutes from "@/app/hooks/useRoutes";
 import DesktopItem from "./DesktopItem";
+import { User } from "@prisma/client";
+import Avatar from "../Avatar";
+
+interface DesktopSidebarProps {
+  currentUser: User;
+}
 
 
-const DesktopSidebar = () => {
+const DesktopSidebar: React.FC<DesktopSidebarProps> = ({
+  currentUser
+}) => {
   const routes = useRoutes();
   const [isOpen, setIsOpen] = useState(false);
+  console.log({currentUser});
   return (
     <div className="hidden
     lg:fixed
@@ -36,6 +45,13 @@ const DesktopSidebar = () => {
             />
           ))}
         </ul>
+      </nav>
+      <nav className="mt-4 flex flex-col justify-between items-center">
+        <div onClick={() => setIsOpen(true)}
+        className="cursor-pointer hover:opacity-75 transition"
+        >
+          <Avatar user={currentUser} />
+        </div>
       </nav>
     </div>
   );
---
+++ app/components/sidebar/MobileItem.tsx
@@ -0,0 +1,35 @@
+'use client'
+import Link from 'next/link'
+import clsx from 'clsx'
+
+interface MobileItemProps {
+  href: string;
+  icon: any;
+  active?: boolean;
+  onClick: () => void;
+}
+
+const MobileItem: React.FC<MobileItemProps> = ({
+  href,
+  icon: Icon,
+  active,
+  onClick
+}) => {
+  const handleClick = () => {
+    if (onClick) {
+      return onClick();
+    }
+  };
+
+  return (
+    <Link href={href} onClick={onClick}
+    className={clsx(`flex group gap-x-3 justify-center text-sm leading-6
+    font-semibold p-4 text-gray-500 hover:text-black
+    hover:bg-gray-100 w-full`, active && 'bg-gray-100 text-black')}
+    >
+      <Icon className="h-6 w-6"/>
+    </Link>
+  )
+}
+
+export default MobileItem
