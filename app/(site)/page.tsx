b4d77095564cc93ed83dc80e3449e87cd17b83e1
canozben <canozben@windowslive.com>
2023-06-30 12:49:42 +0300

    body pusher added realtime msg visible

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,18 +3,13 @@ import AuthForm from './components/AuthForm';
 
 export default function Home() {
   return (
-    <div
-      className="
-      flex flex-col min-h-full
-      bg-gray-100 justify-center py-12 sm:px-6 lg:px-8
-      ">
+    <div className="flex flex-col min-h-full bg-gray-100 justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <DiCssdeck size="4rem"
           height="48"
           width="48"
           className="mx-auto w-auto
-          text-indigo-600"
-        />
+          text-indigo-600"/>
         <h2 className=" mt-6 text-center text-3xl font-bold
           tracking-tight text-gray-900">
             Sign In
